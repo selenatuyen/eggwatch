4aa7dd2b59a11c902abf0650a07c095ec5f766ba
selenatuyen <selenaltuyen@gmail.com>
2020-10-18 17:01:35 -0700

    Add StyledItem and Item

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {StyledCanvas} from './styles/StyledCanvas';
 import Poop from './Poop';
+import Item from './Item';
 
 const Canvas = ({canvas}) => (
     <StyledCanvas height="700px" width="700px">
         <Poop />
+        <Item />
     </StyledCanvas>
 )
 
